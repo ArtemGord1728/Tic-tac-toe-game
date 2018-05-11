fd6a12b5f73f9eed93935eb198e60205db207a70
artemakimov01 <artemakimov01@mail.ru>
2018-05-11 21:34:44 +0300

    Fixed bugs in the code

--- Игра Крестики-нолики/MainCode.cpp
+++ Игра Крестики-нолики/MainCode.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 
 
-#define mapSize 3 // field size
+#define MapSize 3 // field size
 #define NULL_FIELD 0  // empty cell symbol
 #define symbol_cross 88  // symbol of the cross
 #define symbol_zero 79  // noch symbol
@@ -14,7 +14,7 @@ class Tic_tac_toe_Game
 {
 public:
 	short int X, Y, Nmove = 1;
-	static const short int A = mapSize, B = mapSize;
+	static const short int A = MapSize, B = MapSize;
 	char MAP[A][B];
 	char whoMove = NULL_FIELD, win = NULL_FIELD;
 	std::string exitGame = "Yes";
